03b666aebd975fd2a9d32f0cdcf8e2b9cf9f59d8
alex1431999 <ah01242@surrey.ac.uk>
2019-11-09 22:26:02 +0000

    Renamed package to common

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 
 setuptools.setup(
     name='final-year-project-commong',
-    version='0.0.1',
+    version='0.0.2',
     description='The library of all common functionality within the project',
     url='git@gitlab.com:alex1431999/final-year-project-common.git',
     author='Alexander Haller',
