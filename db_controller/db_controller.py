a8ebdf34f8a3f174d40aa9217cf6751233d681bf
alex1431999 <ah01242@surrey.ac.uk>
2019-11-09 22:22:53 +0000

    Added DB controller package

---
+++ db_controller/db_controller.py
@@ -0,0 +1,13 @@
+"""
+Module to implement the database interaction functionality
+"""
+
+class DBController():
+    """
+    Class to handle the database interactions
+    """
+    def __init__(self):
+        pass
+
+    def test_print(self):
+        print('Some text')
