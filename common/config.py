c27752294b6da15e8e3f01e721c26d84368e34c0
alex1431999 <ah01242@surrey.ac.uk>
2019-11-10 15:08:31 +0000

    Made config a python file instead of JSONs

---
+++ common/config.py
@@ -0,0 +1,31 @@
+"""
+Define all the different configurations
+"""
+
+ENTITI_TYPES = [
+    "UNKNOWN",
+    "PERSON",
+    "LOCATION",
+    "ORGANIZATION",
+    "EVENT",
+    "WORK_OF_ART",
+    "CONSUMER_GOOD",
+    "OTHER",
+    "PHONE_NUMBER",
+    "ADDRESS",
+    "DATE",
+    "NUMBER",
+    "PRICE"
+]
+
+SUPPORTED_LANGUAGES = [
+    "zh",
+    "en",
+    "fr",
+    "de",
+    "it",
+    "ja",
+    "ko",
+    "pt",
+    "es"
+]
