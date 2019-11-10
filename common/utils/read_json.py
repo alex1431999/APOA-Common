d7048ceea29eb4039793dd14242e060d24470401
alex1431999 <ah01242@surrey.ac.uk>
2019-11-10 14:54:31 +0000

    Added read_json

---
+++ common/utils/read_json.py
@@ -0,0 +1,15 @@
+"""
+JSON read function to import json files
+"""
+
+import json
+
+def read_json(path):
+    """
+    Read in a JSON file and return the content as dict
+
+    :param str path: The path to the JSON file
+    """
+    with open(path) as file_opened:
+        json_data = json.load(file_opened)
+    return json_data
