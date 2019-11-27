1793d04737ddbcfcec0b3d0a5c9f32d9d2ce4210
alex1431999 <ah01242@surrey.ac.uk>
2019-11-27 23:51:14 +0000

    Add neutral crawl type

--- common/mongo/data_types/crawling/enums/crawl_types.py
+++ common/mongo/data_types/crawling/enums/crawl_types.py
@@ -4,6 +4,7 @@ class CrawlTypes(Enum):
     """
     Enumerate all the different types of crawl sources the system supports
     """
+    NEUTRAL = 'Neutral' # For creating an instance of the crawl parent class
     TWITTER = 'Twitter'
     NEWS = 'News'
     NYT = 'New York Times'
