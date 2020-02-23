89b977ed4d7fb01cd96b9b61b10333513e91aa29
alex1431999 <ah01242@surrey.ac.uk>
2020-02-23 10:05:44 +0000

    Bug fix
    Add missing comma

--- common/celery/__init__.py
+++ common/celery/__init__.py
@@ -4,6 +4,6 @@ This file defines constants related to celery functionality
 
 queues = {
     'twitter': 'twitter-tasks',
-    'news': 'news-tasks'
+    'news': 'news-tasks',
     'processor': 'processor-tasks',
 }
