93a87f6edfb1ac20f20f46d563ded622de24678e
alex1431999 <ah01242@surrey.ac.uk>
2020-03-04 11:22:33 +0000

    Add nyt celery queue

--- common/celery/__init__.py
+++ common/celery/__init__.py
@@ -5,5 +5,6 @@ This file defines constants related to celery functionality
 queues = {
     'twitter': 'twitter-tasks',
     'news': 'news-tasks',
+    'nyt': 'nyt-tasks',
     'processor': 'processor-tasks',
 }
